e2c234367ade19584bde566a4c75cd36b782cd74
Fang Wang <fwangboulder@gmail.com>
2017-01-19 18:07:02 -0800

    add signup for users

--- shoppingListApp/main.py
+++ shoppingListApp/main.py
@@ -8,6 +8,10 @@
 # make more readable code
 # more secure websites
 # html that is easier to modify
+#########
+#for signup HW2##
+import re
+from string import letters
 
 
 import os
@@ -88,6 +92,7 @@ class  FizzBuzzHandler(Handler):
             n=self.request.get('n',0)
             n=n and int(n)
             self.render('fizzbuzz.html',n=n)
+#check localhost:8080/rot13 to see the ROT13
 class Rot13(Handler):
     def get(self):
         self.render("ROT13.html")
@@ -98,5 +103,66 @@ class Rot13(Handler):
             rot13=text.encode('rot13')
         self.render('ROT13.html',text=rot13)
 
+#######################################
+#for Homework 2 signup
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
 
-app=webapp2.WSGIApplication([('/',MainPage),('/fizzbuzz',FizzBuzzHandler),('/rot13',Rot13)], debug=True)
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class Signup(Handler):
+
+    def get(self):
+        self.render("signup.html")
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        params = dict(username = username,
+                      email = email)
+
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup.html', **params)
+        else:
+            self.redirect('/welcome?username=' + username)
+
+class Welcome(Handler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else:
+            self.redirect('/signup')
+
+
+#######################################
+
+
+
+app=webapp2.WSGIApplication([('/',MainPage),('/fizzbuzz',FizzBuzzHandler),('/rot13',Rot13),('/signup',Signup),('/welcome',Welcome)], debug=True)
