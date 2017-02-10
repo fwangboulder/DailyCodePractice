5a6a3e0f8b7aa0bcf36c35387fc1850281ada4b5
Fang Wang <fwangboulder@gmail.com>
2017-02-10 09:57:51 -0800

    feat: hello world flask app

---
+++ framework/flask/project0.py
@@ -0,0 +1,15 @@
+#flask hello world web app
+from flask import Flask
+
+#create an instance of the Flask class.
+app = Flask(__name__)
+
+#decorator wrap functions into app.route function
+@app.route('/')
+@app.route('/hello')
+def HelloWorld():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=9000)
